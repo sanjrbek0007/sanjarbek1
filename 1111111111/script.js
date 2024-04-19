3f367802f5ca1d08f196a27ae6824e7473d64e7c
sanjrbek0007 <151602914+sanjrbek0007@users.noreply.github.com>
2024-04-19 12:02:14 +0500

    Add files via upload

--- 1111111111/script.js
+++ 1111111111/script.js
@@ -1,15 +1,22 @@
 let red = document.getElementById('red')
 let orange = document.getElementById('orange')
 let black = document.getElementById('black')
+let img = document.getElementsByTagName('img')[0]
 
 let btn = document.querySelector('button')
+let btnHover =document.getElementsByTagName('button:hover') 
 
 red.addEventListener('click',() => {
     btn.style.backgroundColor='red'
+    img.setAttribute('src', "/image2 (2).png")
 }) 
 orange.addEventListener('click',() => {
     btn.style.backgroundColor='orange'
+    img.setAttribute('src', "/image3 (2).png")
+
 }) 
 black.addEventListener('click',() => {
     btn.style.backgroundColor='black'
+    img.setAttribute('src', "/pngwing (2).png")
+
 }) 
